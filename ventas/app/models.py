df722c050e9c435eb2ed83e53cb511258a242460
= <=>
2022-04-24 19:32:43 -0400

    hola

--- ventas/app/models.py
+++ ventas/app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.forms import CharField
 
 # Create your models here.
+class Producto(models.Model):
+    id_producto = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+    precio = models.FloatField(default= 0)
+    
+     
