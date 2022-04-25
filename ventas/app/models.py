09b813f6b38e8040e5737ec149782cf297be10a4
= <=>
2022-04-24 23:51:05 -0400

    hola5

--- ventas/app/models.py
+++ ventas/app/models.py
@@ -1,12 +1,21 @@
 from django.db import models
-from django.forms import CharField
 
 # Create your models here.
+
+
+
+class Categoria(models.Model):
+    id_categoria = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50)
+
+    class Meta:
+        db_table='CATEGORIA'
+
 class Producto(models.Model):
     id_producto = models.AutoField(primary_key=True)
     nombre = models.CharField(max_length=50)
     precio = models.FloatField(default= 0)
-    
+    categoria = models.ForeignKey(Categoria,on_delete=models.CASCADE)
     class Meta:
         db_table = "PRODUCTO"
-     
+
