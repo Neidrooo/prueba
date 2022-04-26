4ed9390f75a457e5a996a23138af41ef4305fc4d
= <=>
2022-04-26 14:17:12 -0400

    quinto

--- ventas/app/urls.py
+++ ventas/app/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     # path('<Producto_id>/',views.nombreProducto,name='producto'),
     # path('primera/',views.mostrarTemplate,name='mostrarTemplate'),
     path('mostrar/',views.mostrar,name='mostrar'),
+    path('errores/',views.errores,name='errores'),
+    path('<int:id>/entrada/',views.entrada,name='entrada'),
+    path('<int:id>/entrada_error/',views.entrada_error,name='entrada_error'),
+    path('mostrar_subconjunto/',views.mostrar_subconjunto,name='mostrar_subconjunto'),
 ]
--- ventas/app/views.py
+++ ventas/app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse, response
 from .models import Producto,Categoria
 
@@ -18,3 +18,24 @@ def mostrarTemplate(request):
 def mostrar(request):
     productos = Producto.objects.all()
     return render(request,'mostrar.html',{'productos':productos})
+
+def errores(request):
+    try:
+        producto = Producto.objects.get(pk=8)
+        return render(request,'errores.html',{'producto':producto})
+    except:
+        return HttpResponse('ERROR HERMANITO MIO')
+    
+def entrada(request,id):
+    producto = Producto.objects.get(pk=id)
+    return render(request,'entrada.html',{'producto':producto})
+
+def entrada_error(request,id):
+    producto = get_object_or_404(Producto,pk=id)
+    return render(request,'entrada_error.html',{'producto':producto})
+
+def mostrar_subconjunto(request):
+    productos = Producto.objects.all()
+    return render(request,'mostrar_subconjunto',{'productos':productos})
+
+    
